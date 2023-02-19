5c7fc0ec2c7de068e872f56db826e09156cd8d06
SandrineZeiter <sandrine.zeiter@unifr.ch>
2023-02-19 13:18:24 +0100

    Working version with optimized code. Had multiple pipelines (p1 and gs) and it works much better with the gs version (line 65), as it is faster for the analysis.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # ----------------------- imports -----------------------
 # imports for flask
-from PIL.Image import Image
+import cProfile
+import timeit
+
 from flask import request, Flask
 
 # imports for ML
 import random
+
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.pipeline import make_pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
+from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
@@ -51,8 +55,10 @@ vectorizer = TfidfVectorizer(analyzer='word', token_pattern=r'\b[a-zA-Z]{3,}\b',
                              min_df=5, max_df=0.7, stop_words='english')
 vectorizer.fit_transform(train['tweet'])
 
+rdf = RandomForestClassifier()
 mnb = MultinomialNB(alpha=0.1)
 p1 = make_pipeline(vectorizer, mnb)
+# p1 = make_pipeline(vectorizer, rdf)
 
 alpha_grid = np.logspace(-3, 0, 4)  # Is smoothing parameter for the counts
 param_grid = [{'multinomialnb__alpha': alpha_grid}]
@@ -60,27 +66,41 @@ gs = GridSearchCV(p1, param_grid=param_grid, cv=5, return_train_score=True)
 
 gs.fit(train.tweet, train.feeling)
 
-
 # ----------------------- LIME -----------------------
+
+# Initialization phase
+explainer = LimeTextExplainer(class_names=class_names)
+# p1 = Pipeline([
+#     ('vect', CountVectorizer()),
+#     ('tfidf', TfidfTransformer()),
+#     ('clf', MultinomialNB(alpha=1))
+# ])
+# p1.fit(train.tweet, train.sub_category)
+
 
 def lime_testing_userinput(userinput):
     # creating explainer
-    explainer = LimeTextExplainer(class_names=class_names)
-    p1.fit(train.tweet, train.sub_category)
-    exp = explainer.explain_instance(userinput, p1.predict_proba, num_features=5, top_labels=len(class_names))
+    # explainer = LimeTextExplainer(class_names=class_names)
+    # p1.fit(train.tweet, train.sub_category)
+    exp = explainer.explain_instance(userinput,
+                                     # p1.predict_proba,
+                                     gs.predict_proba,
+                                     num_samples=5000,
+                                     num_features=5,
+                                     top_labels=len(class_names))
     # print(userinput)
     # get index of the predicted class name
     class_index = exp.available_labels()[0]
     # print("Class index ", class_index)
 
     prediction = class_names[class_index]
-    print("Prediction ", prediction)
+    # print("Prediction ", prediction)
 
     # Get the explanations
     explanations = exp.as_list(label=class_index)
     explanations_as_array = np.array(explanations)
     # print("Explanations: ", explanations)
-    print("Explanations as array: ", explanations_as_array)
+    # print("Explanations as array: ", explanations_as_array)
     # print('Explanation for class %s' % class_names[class_index])
     # print('\n'.join(map(str, exp.as_list(label=class_index))))
 
@@ -90,7 +110,8 @@ def lime_testing_userinput(userinput):
     figure = plt.savefig("figure.png", bbox_inches="tight")
     # plt.show()
 
-    return prediction, explanations_as_array, figure
+    return prediction, explanations_as_array
+    # , figure
 
 
 # ----------------------- Flask app -----------------------
@@ -109,13 +130,14 @@ def webhook():
     req = request.get_json(silent=True, force=True)
 
     fulfillment_text = ''
-    fulfillment_image = None
+    # fulfillment_image = None
     query_result = req.get('queryResult')
+
+    action = query_result.get('action')
     # print(query_result)
 
-    if query_result.get('action') == 'getname':
-        # print("Person, name:", query_result.get('parameters').get('person').get('name'))
-        # name = query_result.get('parameters').get('person').get('name')
+    if action == 'getname':
+
         if not query_result.get('parameters').get('person').get('name'):
             fulfillment_text = "Hi, nice to meet you. Please, tell me something about your day."
 
@@ -124,87 +146,56 @@ def webhook():
             # print(name)
             fulfillment_text = "Hi " + name + ", nice to meet you. Please, tell me something about your day."
 
-    elif query_result.get('action') == 'getinformation':
+    elif action == 'getinformation':
         userinput = query_result["queryText"]
-        text_to_analyze = text_to_analyze + " " + userinput
+        text_to_analyze += " " + userinput
 
         # Make sure to get enough data to analyze.
-        if len(text_to_analyze) < 200:
-            possible_answers = ["Please, tell me more.", "Please, let me know more about that.",
+        if len(text_to_analyze) < 256:
+            possible_answers = ["Please, tell me more about that.", "Can you provide me more information about that?",
                                 "I would like to get to know you better, so, can you tell me more?",
+                                "I'm curious to learn more about that",
                                 "To get to know you better, I still need some information",
-                                "Thank you " + name + ". But could you tell me more?"]
+                                "Thanks for sharing, " + name + ". Can you expand on that?"]
 
             fulfillment_text = random.choice(possible_answers)
-            fulfillment_image = None
+            # fulfillment_image = None
 
         else:
-            # text_to_analyze = text_to_analyze[:180]
-            print(len(text_to_analyze))
-            prediction, explanations, figure = lime_testing_userinput(text_to_analyze)
-            words = np.array([])
-            weights = np.array([])
-            for i in range(len(explanations)):
-                for k in range(2):
-                    # print("Position: ", i, k, explanations[i][k])
-                    if k == 0:
-                        words = np.append(words, explanations[i][k])
-                    else:
-                        weights = np.append(weights, explanations[i][k])
+            prediction, explanations = lime_testing_userinput(text_to_analyze)
 
-            # print("Words:", words)
-            # print("Weightings:", weights)
+            # Build the np.arrays for words and weights
+            words = explanations[:, 0].flatten()
+            weights = explanations[:, 1].flatten()
 
+            # Build np.arrays for positive and negative words
             weights = weights.astype(float)
-            negative_idx = np.argwhere(weights < 0)
-            positive_idx = np.argwhere(weights > 0)
-            negative_words = np.array([])
-            positive_words = np.array([])
+            negative_idx = np.where(weights < 0)[0]
+            positive_idx = np.where(weights > 0)[0]
+            negative_words = words[negative_idx]
+            positive_words = words[positive_idx]
 
-            if negative_idx.size != 0:
-                negative_idx = np.concatenate(negative_idx)
-                for i in negative_idx:
-                    negative_words = np.append(negative_words, words[i])
+            # Create fulfillment text for the chatbot
+            fulfillment_text = "Thank you {} for telling me about your day. According to what you said, " \
+                               "you feel {}. The words in favor of my choice for your feeling are " \
+                               "\"{}\"".format(name, prediction, '", "'.join(str(word) for word in positive_words))
 
-                print(negative_words)
-                # print(negative_idx)
-                for all in negative_idx:
-                    print("Negative weights:", weights[all])
-
-            if positive_idx.size != 0:
-                positive_idx = np.concatenate(positive_idx)
-                for i in positive_idx:
-                    positive_words = np.append(positive_words, words[i])
-
-                print(positive_words)
-                # print(positive_idx)
-                for all in positive_idx:
-                    print("Positive weights:", weights[all])
-
-            if negative_idx.size == 0:
-                fulfillment_text = "Thank you " + name + " for telling me about your day. " \
-                                                         "According to what you said, you feel " + prediction + \
-                                   ". The words in favour of my choice for your feeling are " + '"' \
-                                   + '", "'.join([str(word) for word in positive_words]) + '"'
-            else:
-                fulfillment_text = "Thank you " + name + " for telling me about your day. " \
-                                                         "According to what you said, you feel " + prediction + \
-                                   ". The words in favour of my choice for your feeling are " + '"' \
-                                   + '", "'.join([str(word) for word in positive_words]) \
-                                   + '". The words that do not support my choice are: ' + '"'\
-                                   + '", "'.join([str(word) for word in negative_words]) + '".'
-                print(len(fulfillment_text))
+            if negative_idx.size > 0:
+                fulfillment_text += ". The words that do not support my choice are: \"{}\".".format(
+                    '", "'.join(str(word) for word in negative_words))
 
             # fulfillment_text = "Thank you " + name + " for telling me about your day. According to what you said, you feel " + prediction + "."
             # fulfillment_image = ("/Users/sandrinezeiter/Library/CloudStorage/OneDrive-UniversitédeFribourg/"
             #       "Thesis/Chatty/XAIBot/figure.png")
-            fulfillment_image = "https://upload.wikimedia.org/wikipedia/commons/b/bd/Test.svg"
-            text_to_analyze = ''  # So that I don't have to restart the whole script over and over again.
+            # fulfillment_image = "https://upload.wikimedia.org/wikipedia/commons/b/bd/Test.svg"
+
+            # Set back the global variables
+            text_to_analyze = ''
             name = ''
 
     return {
         "fulfillment_text": fulfillment_text,
-        "fulfillment_image": fulfillment_image,
+        # "fulfillment_image": fulfillment_image,
         "source": "webhookdata"
     }
 
